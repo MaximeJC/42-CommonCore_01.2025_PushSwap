6929bc1727f8c768990573085857d25756760991
Maxime Gouraud <mgouraud@c1r2p4.42lehavre.fr>
2025-01-13 13:55:51 +0100

    Add is_sorted

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,10 +6,27 @@
 /*   By: mgouraud <mgouraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 13:44:21 by mgouraud          #+#    #+#             */
-/*   Updated: 2025/01/13 13:55:21 by mgouraud         ###   ########.fr       */
+/*   Updated: 2025/01/13 13:55:35 by mgouraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	is_sorted(t_list *lst)
+{
+	int		highest;
+	t_list	*el;
 
+	el = lst;
+	highest = el->content;
+
+	while (el->next != NULL)
+	{
+		el = el->next;
+		if (el->content > highest)
+			highest = el->content;
+		else
+			return (0);
+	}
+	return (1);
+}
