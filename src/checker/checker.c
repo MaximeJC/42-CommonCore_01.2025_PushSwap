dde63b0b0c4b8b3129ff6485bc06132d54d6d7d1
Maxime Gouraud <mgouraud@c1r2p4.42lehavre.fr>
2025-01-20 15:10:10 +0100

    Checker working, norm OK

--- src/checker/checker.c
+++ src/checker/checker.c
@@ -6,21 +6,22 @@
 /*   By: mgouraud <mgouraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 16:46:10 by mgouraud          #+#    #+#             */
-/*   Updated: 2025/01/14 18:26:04 by mgouraud         ###   ########.fr       */
+/*   Updated: 2025/01/20 15:08:06 by mgouraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static void	readinstructions(t_list **a, t_list **b);
+static int	treatinstruction(t_list **a, t_list **b, char *instruction);
+
 int	main(int argc, char *argv[])
 {
 	char	**args;
-	char	*instruction;
 	t_list	*a;
 	t_list	*b;
 
 	args = NULL;
-	instruction = NULL;
 	a = NULL;
 	b = NULL;
 	if (argc == 1)
@@ -30,43 +31,65 @@ int	main(int argc, char *argv[])
 	else
 		args = argvtoargs(argc, argv);
 	argstostack(args, &a);
-	// instruction = get_next_line(0);
-	while (1 == 1)
-	{
-		instruction = get_next_line(0);
-		ft_printf("-> %s", instruction);
-		if (instruction == NULL)
-			break;
-		if (!ft_strncmp(instruction, "sa", 2))
-			sa(&a);
-		else if (!ft_strncmp(instruction, "sb", 2))
-			sb(&b);
-		else if (!ft_strncmp(instruction, "ss", 2))
-			ss(&a, &b);
-		else if (!ft_strncmp(instruction, "pa", 2))
-			pa(&a, &b);
-		else if (!ft_strncmp(instruction, "pb", 2))
-			pb(&a, &b);
-		else if (!ft_strncmp(instruction, "ra", 2))
-			ra(&a);
-		else if (!ft_strncmp(instruction, "rb", 2))
-			rb(&b);
-		else if (!ft_strncmp(instruction, "rr", 2))
-			rr(&a, &b);
-		else if (!ft_strncmp(instruction, "rra", 3))
-			rra(&a);
-		else if (!ft_strncmp(instruction, "rrb", 3))
-			rrb(&b);
-		else if (!ft_strncmp(instruction, "rrr", 3))
-			rrr(&a, &b);
-		else
-			break;
-	}
+	readinstructions(&a, &b);
 	if (is_sorted(a) && b == NULL)
 		ft_printf("OK\n");
 	else
 		ft_printf("KO\n");
 	ft_lstclear(&a);
 	ft_lstclear(&b);
+	return (0);
+}
+
+static void	readinstructions(t_list **a, t_list **b)
+{
+	char	*instruction;
+
+	instruction = NULL;
+	while (1 == 1)
+	{
+		instruction = get_next_line(0);
+		if (instruction == NULL)
+			break ;
+		if (treatinstruction(a, b, instruction))
+		{
+			if (instruction != NULL)
+			{
+				ft_putstr_fd("Error\n", 2);
+				ft_lstclear(a);
+				ft_lstclear(b);
+				exit(EXIT_FAILURE);
+			}
+			break ;
+		}
+	}
+}
+
+static int	treatinstruction(t_list **a, t_list **b, char *instruction)
+{
+	if (!ft_strncmp(instruction, "sa", 2))
+		sa(a, 0);
+	else if (!ft_strncmp(instruction, "sb", 2))
+		sb(b, 0);
+	else if (!ft_strncmp(instruction, "ss", 2))
+		ss(a, b, 0);
+	else if (!ft_strncmp(instruction, "pa", 2))
+		pa(a, b, 0);
+	else if (!ft_strncmp(instruction, "pb", 2))
+		pb(a, b, 0);
+	else if (!ft_strncmp(instruction, "rra", 3))
+		rra(a, 0);
+	else if (!ft_strncmp(instruction, "rrb", 3))
+		rrb(b, 0);
+	else if (!ft_strncmp(instruction, "rrr", 3))
+		rrr(a, b, 0);
+	else if (!ft_strncmp(instruction, "ra", 2))
+		ra(a, 0);
+	else if (!ft_strncmp(instruction, "rb", 2))
+		rb(b, 0);
+	else if (!ft_strncmp(instruction, "rr", 2))
+		rr(a, b, 0);
+	else
+		return (1);
 	return (0);
 }
